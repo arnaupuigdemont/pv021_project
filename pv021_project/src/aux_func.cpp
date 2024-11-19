fb94f858cac3b0c5b7a8288e98c85e79d3da5e6e
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2024-11-19 19:36:42 +0100

    debug

--- pv021_project/src/aux_func.cpp
+++ pv021_project/src/aux_func.cpp
@@ -2,41 +2,46 @@
 
     // Function to read CSV data
     vector<vector<double>> read_csv(const string& filename) {
+        cout << "Reading vectors from " << filename << " ..." << endl;
         ifstream file(filename);
         string line;
         vector<vector<double>> data;
-
         while (getline(file, line)) {
+            stringstream lineStream(line);
+            string cell;
             vector<double> row;
-            stringstream ss(line);
-            string value;
-
-            while (getline(ss, value, ',')) {
-                row.push_back(stod(value));
+            while (getline(lineStream, cell, ',')) {
+                row.push_back(stod(cell));
             }
             data.push_back(row);
         }
-
+        if (data.empty()) {
+            cerr << filename << " is empty or could not be read." << endl;
+        }
         return data;
     }
 
     // Function to load labels from a CSV file
     vector<int> load_labels(const string& filename) {
+        cout << "Reading labels from " << filename << " ..." << endl;
         ifstream file(filename);
         string line;
-        vector<int> labels;
-
+        vector<int> data;
         while (getline(file, line)) {
-            labels.push_back(stoi(line));
+            data.push_back(stoi(line));
         }
-
-        return labels;
+        if (data.empty()) {
+            cerr << filename << " is empty or could not be read." << endl;
+        }
+        return data;
     }
 
     // Function to write predictions to a CSV file
     void write_predictions(const string& filename, const vector<int>& predictions) {
+        cout << "Writing predictions to " << filename << " ..." << endl;
         ofstream file(filename);
-        for (const int& pred : predictions) {
-            file << pred << "\n";
+        for (int val : predictions) {
+            file << val << endl;
         }
+        file.close();
     }
