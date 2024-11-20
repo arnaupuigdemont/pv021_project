4090c5d4608f4d77e7c2225eb9fa4ebefc844593
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2024-11-20 14:33:43 +0100

    couts

--- pv021_project/src/normalization_func.cpp
+++ pv021_project/src/normalization_func.cpp
@@ -3,16 +3,15 @@
 using namespace std;
 
 pair<vector<double>, vector<double>> find_min_max(const vector<vector<double>> &data) {
-    cout << "2" << endl;
+    
     cout << "data size: " << data.size() << endl;
     vector<double> minValues = data[0];
-    cout << "3" << endl;
     vector<double> maxValues = data[0];
-    cout << "1 " << data.size() << " " << data[0].size() << endl;
+
     for (long unsigned int i = 1; i < data.size(); ++i) {
-        cout << i << endl;
+
         for (long unsigned int j = 0; j < data[0].size(); ++j) {
-            cout << j << endl;
+            
             if (data[i][j] < minValues[j]) {
                 minValues[j] = data[i][j];  
             }
