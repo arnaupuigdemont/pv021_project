38be827c02026716bdadbfcc3f63c15e02dacb70
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2024-12-28 19:10:25 +0100

    hyper

--- pv021_project/src/main.cpp
+++ pv021_project/src/main.cpp
@@ -15,11 +15,13 @@
 using namespace std;
 
 const int OUTPUT_SIZE = 10;
-const int EPOCHS = 20;
-double initial_lr = 0.001; 
-double decay_rate = 0.1;
-const int BATCH_SIZE = 128;
-int lambda = 0.0001;
+const int EPOCHS = 20; //20
+double initial_lr = 0.005; //0.001
+double decay_rate = 0.1; //0.1
+const int BATCH_SIZE = 128; //128
+int lambda = 0.001; //0.0001
+
+//86.1%
 
 Matrix to_one_hot(int label, int num_classes) {
     std::vector<double> one_hot(num_classes, 0.0);
