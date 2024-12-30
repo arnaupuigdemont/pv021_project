cfaf0025e3f652d207870606d79c3341d9cf1e47
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2024-12-30 13:28:10 +0100

    no decay

--- pv021_project/src/main.cpp
+++ pv021_project/src/main.cpp
@@ -62,7 +62,7 @@ int main() {
 
             auto epoch_start = std::chrono::high_resolution_clock::now();
 
-            double learning_rate = initial_lr;
+           // double learning_rate = initial_lr;
 
             double total_loss = 0.0;
             int correct_predictions = 0;
@@ -149,10 +149,10 @@ int main() {
                 //grad = grad.clip_gradients(-25.0, 25.0);
 
                 //SGD MOMENTUM
-                Matrix grad = output_layer.backward_SGD_Momentum(grad_output, learning_rate, 0.9);
-                grad = hidden_layer3.backward_SGD_Momentum(grad, learning_rate, 0.9);
-                grad = hidden_layer2.backward_SGD_Momentum(grad, learning_rate, 0.9);
-                grad = input_layer.backward_SGD_Momentum(grad, learning_rate, 0.9);
+                Matrix grad = output_layer.backward_SGD_Momentum(grad_output, initial_lr, 0.9);
+                grad = hidden_layer3.backward_SGD_Momentum(grad, initial_lr, 0.9);
+                grad = hidden_layer2.backward_SGD_Momentum(grad, initial_lr, 0.9);
+                grad = input_layer.backward_SGD_Momentum(grad, initial_lr, 0.9);
 
             }
 
