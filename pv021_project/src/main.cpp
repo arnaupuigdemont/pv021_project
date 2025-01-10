77797ab579fc4e673e284d8223895232e9589ccf
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2025-01-10 16:31:03 +0100

    debug

--- pv021_project/src/main.cpp
+++ pv021_project/src/main.cpp
@@ -57,7 +57,7 @@ int main() {
 
             auto epoch_start = std::chrono::high_resolution_clock::now();
             
-            if(epoch % 10 == 0) learning_rate /= initial_rate ;
+            //if(epoch % 10 == 0) learning_rate /= initial_rate ;
             std::cout << "Learning rate: " << learning_rate << std::endl;
 
             double total_loss = 0.0;
