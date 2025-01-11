e1b78b9a550e1404f5da716290c2078c4ec23f50
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2025-01-11 21:14:32 +0100

    s

--- pv021_project/src/activationFunction.hpp
+++ pv021_project/src/activationFunction.hpp
@@ -4,20 +4,25 @@
 
 class ActivationFunction {
 
-// Leaky ReLU activation function applied element-wise on a single value.
-valueType leakyReLu(valueType x, float alpha);
+public:
 
-// Applies the Leaky ReLU activation on all elements of the input vector.
-Vector leakyReLu(const Vector &inputVector, float alpha);
+    //default constructor
+    ActivationFunction() = default;
 
-// Applies the Softmax function on the input vector.
-Vector softmax(const Vector &inputVector);
+    // Leaky ReLU activation function applied element-wise on a single value.
+    valueType leakyReLu(valueType x, float alpha);
 
-// Computes the derivative of the Leaky ReLU activation (element-wise).
-Vector leakyReLuDerivative(const Vector &inputVector, float alpha);
+    // Applies the Leaky ReLU activation on all elements of the input vector.
+    Vector leakyReLu(const Vector &inputVector, float alpha);
 
-// Computes the derivative of the Softmax function.
-// (Often this is computed together with the loss, but here it is defined as a separate function.)
-Vector softmaxDerivative(const Vector &inputVector);
+    // Applies the Softmax function on the input vector.
+    Vector softmax(const Vector &inputVector);
+
+    // Computes the derivative of the Leaky ReLU activation (element-wise).
+    Vector leakyReLuDerivative(const Vector &inputVector, float alpha);
+
+    // Computes the derivative of the Softmax function.
+    // (Often this is computed together with the loss, but here it is defined as a separate function.)
+    Vector softmaxDerivative(const Vector &inputVector);
 };
 #endif
