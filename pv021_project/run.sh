7afb19cbe3fe921bd0818ec0d3b4408d7f64cba8
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2025-01-11 18:44:56 +0100

    d

--- pv021_project/run.sh
+++ pv021_project/run.sh
@@ -7,7 +7,7 @@ echo "    COMPILING    "
 echo "#################"
 
 # g++ -Wall -std=c++17 -O3 src/main.cpp src/file2 -o network
-g++ -Wall -Werror -std=c++17 src/main.cpp src/matrix.cpp src/dataset.cpp src/network.cpp -g -o network -Ofast -ffp-contract=fast -funsafe-math-optimizations -march=native -msse2 -mfpmath=sse -ftree-vectorize -fopenmp
+g++ -Wall -Werror -std=c++17 src/main.cpp src/matrix.cpp src/dataset.cpp src/network.cpp src/layer.cpp -g -o network -Ofast -ffp-contract=fast -funsafe-math-optimizations -march=native -msse2 -mfpmath=sse -ftree-vectorize -fopenmp
 
 echo "#################"
 echo "     RUNNING     "
