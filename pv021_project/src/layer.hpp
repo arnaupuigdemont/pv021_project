979ef034048eceb21bc854eb722b6946da649411
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2025-01-11 20:16:46 +0100

    d

--- pv021_project/src/layer.hpp
+++ pv021_project/src/layer.hpp
@@ -1,54 +1,47 @@
 #ifndef LAYER_HPP
 #define LAYER_HPP
 
-
 #include "matrix.hpp"
 #include <vector>
 #include <cstddef>
 
-// Renombramos la función 'initializeWeights' a 'initWeights'
+// Renamed 'initializeWeights' to 'initWeights'
 Matrix initWeights(int inDim, int outDim, bool output, bool uniformDist = true);
 
-// Renombramos la función 'initializeBias' a 'initBias'
+// Renamed 'initializeBias' to 'initBias'
 Vector initBias(int dimension);
 
 class Layer {
 
-    friend class MLP; // MLP puede acceder a sus miembros privados
+    friend class MLP; // MLP can access its private members
 
 private:
-
     bool output;
+    int _dimension;  // previously _activationFunction
 
-    Vector _outputs;           // antes _values
-    Vector _valDerivs;         // antes _valuesDerivatives
+    Vector _outputs;           // previously _values
+    Vector _valDerivs;         // previously _valuesDerivatives
 
     Matrix _weights;
     Vector _bias;
 
-    Matrix _grads;             // antes _gradients
-    Vector _biasGrads;         // antes _biasGradients
+    Matrix _grads;             // previously _gradients
+    Vector _biasGrads;         // previously _biasGradients
 
     Vector _deltas;
 
-    Matrix _m_w_adam;
-    Matrix _v_w_adam;
-    Vector _m_b_adam;
-    Vector _v_b_adam;
+    Matrix _m_w_adam;          // previously _adamFirstMoment
+    Matrix _v_w_adam;          // previously _adamSecondMoment
+    Vector _m_b_adam;          // previously _adamBiasFirstMom
+    Vector _v_b_adam;          // previously _adamBiasSecondMom
 
-    const valueType beta1 = 0.9;
-    const valueType beta2 = 0.999;
-    const valueType eps   = 1e-8;
+    Matrix _sgdVelocity;
+    Vector _sgdBiasVelocity;
 
-	Matrix _sgdVelocity;
-	Vector _sgdBiasVelocity;
+    valueType _leakyAlpha;     // previously _leakyReLUAlpha
 
-    int    _dimension;  // antes _activationFunction
-
-    valueType _leakyAlpha;     // antes _leakyReLUAlpha
-
-    // Renombramos 'useActivationFunction' y 'useDerivedActivationFunction'
-    // a algo más directo: 'applyActivation' y 'applyActivationDeriv'
+    // Renamed 'useActivationFunction' and 'useDerivedActivationFunction'
+    // to something more direct: 'applyActivation' and 'applyActivationDeriv'
     Vector applyActivation(const Vector &inputVec);
     Vector applyActivationOutput(const Vector &inputVec);
     Vector applyActivationDeriv(const Vector &inputVec);
@@ -56,7 +49,9 @@ private:
 
 public:
     Layer(int inDim, int outDim, bool output)
-      : _outputs(outDim),
+      : output(output),
+        _dimension(outDim),
+        _outputs(outDim),
         _valDerivs(outDim),
         _weights(initWeights(inDim, outDim, output)),
         _bias(initBias(outDim)),
@@ -67,17 +62,9 @@ public:
         _v_w_adam(inDim, outDim),
         _m_b_adam(outDim),
         _v_b_adam(outDim),
-		_sgdVelocity(inDim, outDim),
-		_sgdBiasVelocity(outDim),      
-        _dimension(outDim),
-        output(output),
+        _sgdVelocity(inDim, outDim),
+        _sgdBiasVelocity(outDim),
         _leakyAlpha(0.01) {}
-
-    const Matrix& getWeights() const { return _weights; }
-    const Vector& getBias()    const { return _bias; }
-    const Vector& getOutputs() const { return _outputs; }
-
-    int getDimension() const { return _dimension; }
-    size_t size()      const { return static_cast<size_t>(_dimension); }
 };
-#endif
+
+#endif // LAYER_HPP
