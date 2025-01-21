598ec0da05bbf5f22dab8222bb9fb41089c43aa9
Arnau Puigdemont <arnau.puigdemont@estudiantat.upc.edu>
2025-01-21 11:51:57 +0100

    lamda 0.05

--- pv021_project/src/network.hpp
+++ pv021_project/src/network.hpp
@@ -9,7 +9,7 @@
 class Network {
 
     private:
-        const double regLambda = 0.08;  
+        const double regLambda = 0.05;  
 
         std::vector<Vector> _trainData;
         std::vector<int>    _trainLabels;
